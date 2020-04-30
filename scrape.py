667084a35ee823fdd81a9b795911373f81eec01f
scrapehero-code <scrapehero@gmail.com>
2020-04-30 10:44:33 +0530

    Removed old code

--- scrape.py
+++ scrape.py
@@ -24,13 +24,6 @@ def scrape(url):
     # Download the page using requests
     print("Downloading %s"%url)
     r = requests.get(url, headers=headers)
-    # Simple check to check if page was blocked (Usually 503)
-    if r.status_code > 500:
-        if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
-        else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
-        return None
     # Pass the HTML of the page and create 
     return e.extract(r.text,base_url=url)
 
